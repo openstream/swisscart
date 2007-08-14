f4189f8258820e7e8505d55744df3d6f05cfe42d
Nick Weisser <nick@openstream.ch>
2007-08-14 22:22:35 +0000

    added language files for new payment module needed in relation with pickup shipping module

---
+++ includes/languages/espanol/modules/payment/cof.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  $Id: cof.php,v 1.8 2003/07/11 09:04:23 http://www.horse-n-rider.de Exp $
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2002 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_PAYMENT_COF_TEXT_TITLE', 'Barzahlung (nur für Selbstabholer!)');
+  define('MODULE_PAYMENT_COF_TEXT_DESCRIPTION', 'Barzahlung');
+?>
