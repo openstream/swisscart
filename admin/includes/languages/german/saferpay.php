67e85faba78101243bd301c6efc9e1631bff3ada
Nick Weisser <nick@openstream.ch>
2007-12-04 19:26:24 +0000

    added language files and buttons for SaferPay transactions admin

---
+++ admin/includes/languages/german/saferpay.php
@@ -0,0 +1,31 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: saferpay.php 1308 2005-10-15 14:22:18Z af $
+
+   Copyright (c) 2006 Alexander Federau
+   -----------------------------------------------------------------------------------------
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+define('TEXT_EDIT_STATUS', 'Status');
+define('HEADING_TITLE', 'Saferpay Transaktionen');
+define('HEADING_TITLE_SEARCH', 'Suche: ');
+define('HEADING_TITLE_DATE_FROM', 'Datum von: ');
+define('HEADING_TITLE_DATE_TO', ' bis: ');
+define('HEADING_TITLE_DATE_FORMAT', '(jjjj-mm-tt)');
+define('HEADING_TITLE_STATUS', 'Status: ');
+define('TEXT_ALL_TRANSACTIONS', 'Alle Transaktionen');
+define('TEXT_RESERVED', 'Reservierte');
+define('TEXT_COMPLETED', 'Verbuchte');
+
+
+define('TABLE_HEADING_ORDER_ID', 'Order ID');
+define('TABLE_HEADING_TRANSACTION_DATE', 'Bestelldatum');
+define('TABLE_HEADING_PROVIDER', 'Verarbeiter');
+define('TABLE_HEADING_SAFERPAY_ID', 'Saferpay ID*');
+define('TABLE_HEADING_ORDER_TOTAL', 'Total');
+define('TABLE_HEADING_SAFERPAY_AMOUNT', 'Betrag');
+define('TABLE_HEADING_SAFERPAY_STATUS', 'Verbucht?');
+define('TEXT_SAFERPAY_ID_LINK', '*Ein Klick auf die Saferpay ID führt zur Detailansicht der Transaktion im Saferpay Backoffice.');
+define('IMAGE_COMPLETE', 'Verbuchen');
+?>
