e7e78859b3850d61907c09d2957ed68d074a485d
Nick Weisser <nick@openstream.ch>
2007-12-11 03:22:26 +0000

    moved language drop down array handling to application_top.php

--- admin/index.php
+++ admin/index.php
@@ -11,17 +11,6 @@
 */
 
   require('includes/application_top.php');
-
-  $languages = tep_get_languages();
-  $languages_array = array();
-  $languages_selected = DEFAULT_LANGUAGE;
-  for ($i = 0, $n = sizeof($languages); $i < $n; $i++) {
-    $languages_array[] = array('id' => $languages[$i]['code'],
-                               'text' => $languages[$i]['name']);
-    if ($languages[$i]['directory'] == $language) {
-      $languages_selected = $languages[$i]['code'];
-    }
-  }
 ?>
 <!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html <?php echo HTML_PARAMS; ?>>
