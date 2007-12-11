df89c449e36bf5cd1605a6c9aa948cbd95c0e575
Nick Weisser <nick@openstream.ch>
2007-12-11 03:21:07 +0000

    removed "unnecessary" features from tinymce advanced theme

--- admin/categories.php
+++ admin/categories.php
@@ -410,16 +410,16 @@ if($_GET['action'] != 'new_product_preview') { // prevent hidden fields to be re
 	echo '",
 	language : "' . $languages_selected . '",
 	theme : "' . TINYMCE_THEME . '",
-	plugins : "table,advhr,advimage,advlink,emotions,preview,flash,print,contextmenu",';
+	plugins : "table,advhr,advimage,advlink,emotions,flash,contextmenu",';
 	// theme_advanced_buttons1_add : "fontselect,fontsizeselect",
-	echo 'theme_advanced_buttons2_add : "separator,preview,separator,forecolor,backcolor",
+	echo 'theme_advanced_buttons2_add : "separator,forecolor",
 	theme_advanced_buttons2_add_before: "cut,copy,paste,separator",
 	theme_advanced_buttons3_add_before : "tablecontrols,separator",
-	theme_advanced_buttons3_add : "emotions,flash,advhr,separator,print",
+	theme_advanced_buttons3_add : "emotions,flash,advhr",
 	theme_advanced_toolbar_location : "top",
 	theme_advanced_toolbar_align : "left",
 	theme_advanced_path_location : "bottom",
-	extended_valid_elements : "a[name|href|target|title|onclick],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name],hr[class|width|size|noshade],font[face|size|color|style],span[class|align|style]",
+	extended_valid_elements : "a[name|href|target|title|onclick],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name],hr[class|width|size|noshade]",
 	external_link_list_url : "example_data/example_link_list.js",
 	external_image_list_url : "example_data/example_image_list.js",
 	flash_external_list_url : "example_data/example_flash_list.js"
