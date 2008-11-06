fab3fd03cd84676f91b9257d77e441caaa5eb35d
Nick Weisser <nick@openstream.ch>
2008-11-06 09:23:47 +0000

    Fixed corrupted characters in zones database table, added moneyorder bank details to config database table

--- includes/languages/german/modules/payment/moneyorder.php
+++ includes/languages/german/modules/payment/moneyorder.php
@@ -11,6 +11,6 @@
 */
 
   define('MODULE_PAYMENT_MONEYORDER_TEXT_TITLE', 'Vorauskasse (Überweisung)');
-  define('MODULE_PAYMENT_MONEYORDER_TEXT_DESCRIPTION', 'Zahlbar an:&nbsp;' . MODULE_PAYMENT_MONEYORDER_PAYTO . '<br>Adressat:<br><br>' . nl2br(STORE_NAME_ADDRESS) . '<br><br>' . 'Ihre Bestellung wird versandt, sobald wir Ihre Zahlung erhalten haben.');
-  define('MODULE_PAYMENT_MONEYORDER_TEXT_EMAIL_FOOTER', "Zahlbar an: ". MODULE_PAYMENT_MONEYORDER_PAYTO . "\n\nAdressat:\n" . STORE_NAME_ADDRESS . "\n\n" . 'Ihre Bestellung wird versandt, sobald wir Ihre Zahlung erhalten haben.');
+  define('MODULE_PAYMENT_MONEYORDER_TEXT_DESCRIPTION', nl2br(BANKDETAILS));
+  define('MODULE_PAYMENT_MONEYORDER_TEXT_EMAIL_FOOTER', BANKDETAILS);
 ?>
