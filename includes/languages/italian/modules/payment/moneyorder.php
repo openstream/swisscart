cb650b23caabf89d98d890179a2e12dd3080a094
Nick Weisser <nick@openstream.ch>
2008-12-15 11:04:28 +0000

    Changed to display bank details from new configuration variable

--- includes/languages/italian/modules/payment/moneyorder.php
+++ includes/languages/italian/modules/payment/moneyorder.php
@@ -10,7 +10,7 @@
   Released under the GNU General Public License 
 */
 
-  define('MODULE_PAYMENT_MONEYORDER_TEXT_TITLE', 'Check/Money Order');
-  define('MODULE_PAYMENT_MONEYORDER_TEXT_DESCRIPTION', 'Da pagare a:&nbsp;' . MODULE_PAYMENT_MONEYORDER_PAYTO . '<br><br>Da Spedire a:<br>' . nl2br(STORE_NAME_ADDRESS) . '<br><br>' . 'Il tuo ordine non verrà spedito finchè non riceveremo il pagamento.');
-  define('MODULE_PAYMENT_MONEYORDER_TEXT_EMAIL_FOOTER', "Da pagare a:&nbsp;". MODULE_PAYMENT_MONEYORDER_PAYTO . "\n\na Spedire a:\n" . STORE_NAME_ADDRESS . "\n\n" . 'Il tuo ordine non verrà spedito finchè non riceveremo il pagamento.');
+  define('MODULE_PAYMENT_MONEYORDER_TEXT_TITLE', 'Prepagamento');
+  define('MODULE_PAYMENT_MONEYORDER_TEXT_DESCRIPTION', nl2br(BANKDETAILS));
+  define('MODULE_PAYMENT_MONEYORDER_TEXT_EMAIL_FOOTER', BANKDETAILS);
 ?>
