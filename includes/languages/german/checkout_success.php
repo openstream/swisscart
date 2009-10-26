5e96d2d883863eb36245f9eb6a321692b9d5e1ae
Nick Weisser <nick@openstream.ch>
2009-10-26 08:25:17 +0000

    Removed shipping info "2-5 days"Ã

--- includes/languages/german/checkout_success.php
+++ includes/languages/german/checkout_success.php
@@ -18,7 +18,7 @@ define('NAVBAR_TITLE_2', 'Erfolg');
 
 define('HEADING_TITLE', 'Ihre Bestellung ist ausgef&uuml;hrt worden.');
 
-define('TEXT_SUCCESS', 'Ihre Bestellung ist eingegangen und wird bearbeitet! Die Lieferung erfolgt innerhalb von ca. 2-5 Werktagen.');
+define('TEXT_SUCCESS', 'Ihre Bestellung ist eingegangen und wird bearbeitet!');
 define('TEXT_NOTIFY_PRODUCTS', 'Bitte benachrichtigen Sie mich &uuml;ber Aktuelles zu folgenden Produkten:');
 define('TEXT_SEE_ORDERS', 'Sie k&ouml;nnen Ihre Bestellung(en) auf der Seite <a href="' . tep_href_link(FILENAME_ACCOUNT, '', 'SSL') . '"><u>\'Ihr Konto\'</a></u> jederzeit einsehen und sich dort auch Ihre <a href="' . tep_href_link(FILENAME_ACCOUNT_HISTORY, '', 'SSL') . '"><u>\'Bestell&uuml;bersicht\'</u></a> anzeigen lassen.');
 define('TEXT_CONTACT_STORE_OWNER', 'Falls Sie Fragen bez&uuml;glich Ihrer Bestellung haben, wenden Sie sich an unseren <a href="' . tep_href_link(FILENAME_CONTACT_US) . '"><u>Vertrieb</u></a>.');
