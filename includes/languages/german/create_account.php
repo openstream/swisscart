94ad9e01e059ab8fe13271970b8b1e378a818794
Nick Weisser <nick@openstream.ch>
2011-05-09 18:54:31 +0000

    Better German translation for welcome e-mail

--- includes/languages/german/create_account.php
+++ includes/languages/german/create_account.php
@@ -19,7 +19,7 @@ define('HEADING_TITLE', 'Informationen zu Ihrem Kundenkonto');
 
 define('TEXT_ORIGIN_LOGIN', '<font color="#FF0000"><small><b>ACHTUNG:</b></font></small> Wenn Sie bereits ein Konto besitzen, so melden Sie sich bitte <a href="%s"><u><b>hier</b></u></a> an.');
 
-define('EMAIL_SUBJECT', 'Willkommen zu ' . STORE_NAME);
+define('EMAIL_SUBJECT', 'Willkommen bei ' . STORE_NAME);
 define('EMAIL_GREET_MR', 'Sehr geehrter Herr ' . stripslashes($HTTP_POST_VARS['lastname']) . ',' . "\n\n");
 define('EMAIL_GREET_MS', 'Sehr geehrte Frau ' . stripslashes($HTTP_POST_VARS['lastname']) . ',' . "\n\n");
 define('EMAIL_GREET_NONE', 'Sehr geehrte ' . stripslashes($HTTP_POST_VARS['firstname']) . ',' . "\n\n");
