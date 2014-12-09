a279163e7daf2673af128d4df687466976a7da81
WebTecConsulting <dev@shop-base.de>
2014-12-09 17:44:59 +0100

    BugFix for Frontend: empty Customeradress

--- includes/register_globals.php
+++ includes/register_globals.php
@@ -2,7 +2,7 @@
 // Since register global is disabled request values are copyed to HTTP_* and global vars
 
 $REGISTER_REQUEST_ARRAY = array( 'action','address','agree','cPath','cart_delete','cart_delete[]','cart_quantity','cart_quantity[]'
-		,'categories_id','city','comments','company','confirmation','country','coupon_code','currency','dfrom','dto','email','email_address'
+		,'categories_id','city','comments','company','confirmation','country','coupon_code','currency','dfrom','dto','edit','email','email_address'
 		,'enquiry','fax','firstname','gender','guest','id','inc_subcat','keywords','language','language_id','lastname','manufacturers_id','name'
 		,'newsletter','newsletter_general','notify[]','number_of_uploads','order_id','osCsid','page','password','payment','pfrom','postcode','product_global'
 		,'products_id','products_id[]','pto','search_in_description','shipping','sort','street_address','telephone','x','y' );
